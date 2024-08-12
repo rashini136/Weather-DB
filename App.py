92ea6f33a8fdfaeb31d00403397d841b876f0a15
rashini136 <56492781+rashini136@users.noreply.github.com>
2024-08-12 20:08:25 +0530

    Update App.py

--- App.py
+++ App.py
@@ -43,7 +43,7 @@ st.markdown("""
 st.markdown("""
     <div style="background-color: #000035; padding: 20px; border-radius: 5px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
         <h2 class="stTitle">Weather Dashboard</h2>
-        <p>Choose your desired location, date range, and latitude/longitude to get weather data.</p>
+        <p> Choose your desired location, date range, latitude and longitude to get weather data </p>
     </div>
     """, unsafe_allow_html=True)
 
